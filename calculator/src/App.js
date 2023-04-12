05091d9585f4acdd344a595578394594a3053f9c
moonyeju <moonyeju8510@gmail.com>
2023-04-12 21:49:08 +0900

    2.7:: 계산기 버튼 만들기

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
-import Button from '../components/Button';
+import Button, { ButtonTypes } from './components/Button';
 
 export default function App() {
   return (
@@ -7,14 +7,25 @@ export default function App() {
       <View style={styles.container}>
         <Text style={styles.txt}>calculator app</Text>
         <Button
+          title="1"
+          onPress={() => {}}
+          buttonStyle={{ width: 100, height: 100 }}
+          buttonType={ButtonTypes.NUMBER}
+        />
+        <Button
+          title="0"
+          onPress={() => {}}
+          buttonStyle={{ width: 200, height: 100 }}
+          buttonType={ButtonTypes.OPERATOR}
+        />
+        {/* <Button
           title="button"
           color={'pink'}
           onPress={() => console.log('click')}
-        />
+        /> */}
         {/* 버튼 생성하면 ios로 보면 배경없는 파란색 글씨로 button
         안드로이드로 보면 배경파랑인 하얀글씨 Button 첫글자 대문자로
         title은 꼭 넣어야함. onpress는 아님*/}
-        <Button />
         {/* title지정 안하고 defaultProps지정해두면 지정한 값 뜸. */}
         {/* TouchableHighlight -> 터치했을때 배경색 바뀜
         TouchableOpacity -> 터치했을때 투명도 조절됨
--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -1,24 +1,40 @@
 import {
   Pressable,
   Text,
+  StyleSheet,
   //   TouchableHighlight,
   //   TouchableOpacity,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
-const Button = ({ title }) => {
+export const ButtonTypes = {
+  NUMBER: 'NUMBER',
+  OPERATOR: 'OPERATOR',
+};
+const Colors = {
+  NUMBER: ['#71717a', '#3f3f46'],
+  OPERATOR: ['#f59e0b', '#b45309'],
+};
+
+const Button = ({ title, onPress, buttonStyle, buttonType }) => {
   return (
     <Pressable
       // 기본적으로 아무 효과도 없음
-      onPress={() => console.log('opacit')}
+      onPress={onPress}
       style={({ pressed }) => [
         // console.log(data); //pressed 객체 전달
         //반드시 리턴 해야함
-        { backgroundColor: 'pink' },
-        pressed && { backgroundColor: 'skyblue', opacity: 0.3 },
+        {
+          backgroundColor: Colors[buttonType][0],
+        },
+        pressed && {
+          backgroundColor: Colors[buttonType][1],
+        },
+        buttonStyle,
+        buttonType,
       ]}
     >
-      <Text>{title}</Text>
+      <Text style={styles.title}>{title}</Text>
     </Pressable>
     // onPressIn -> 버튼을 눌렀을 때
     // onPressOut -> 버튼을 누른 다음 뗐을 때
@@ -40,9 +56,23 @@ const Button = ({ title }) => {
   );
 };
 Button.defaultProps = {
-  title: 'Default',
+  // title: 'Default',
+  buttonType: ButtonTypes.NUMBER,
 };
 Button.PropTypes = {
   title: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired,
+  buttonStyle: PropTypes.object,
+  buttonType: PropTypes.oneOf(Object.values(ButtonTypes)),
 };
+const styles = StyleSheet.create({
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 50,
+    color: '#ffffff',
+  },
+});
 export default Button;
