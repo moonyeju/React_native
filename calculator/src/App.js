ab5904614693e2740f630b604f7f60e43766b339
moonyeju <moonyeju8510@gmail.com>
2023-04-12 22:08:24 +0900

    ~2.9::useState Hook으로 상태관리하기 까지

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,21 +1,37 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Button, { ButtonTypes } from './components/Button';
+import { useState } from 'react';
+//hook 사용규칙
+//1. 함수 컴포넌트와 커스텀 컴포넌트에서만 사용해야 함
+//2. 함수 최상위에서만 사용해야 함 (if문이나 함수 안에서 사용 X 왜? 리랜더링 하면 또 상태가 바뀔 수 있음.그래서 이걸 eslint로 미리 방지가능)
 
 export default function App() {
+  // let result = 0;
+  //const는 변경 불가능 그래서 let씀
+  const [result, setResult] = useState(0);
   return (
     <>
       <View style={styles.container}>
-        <Text style={styles.txt}>calculator app</Text>
+        {/* <Text style={styles.txt}>calculator app</Text> */}
+        <Text>{result}</Text>
         <Button
-          title="1"
-          onPress={() => {}}
+          title="+"
+          onPress={() => {
+            setResult(result + 1); //비동기적 이거 두개 적으면 리랜더링 전이라서 결과는 1만 더해짐
+            // setResult((prev) => { //이전값의 영향을 받아 업데이트할 땐 함수형 업데이트해야함
+            //   return prev + 1; //이거쓰면 2가 올라감 //return 필수
+            // });
+            //이때 부모가 리랜더링하면 자식컴포넌트인 button.js도 리랜더링됨.
+          }}
           buttonStyle={{ width: 100, height: 100 }}
-          buttonType={ButtonTypes.NUMBER}
+          buttonType={ButtonTypes.OPERATOR}
         />
         <Button
-          title="0"
-          onPress={() => {}}
-          buttonStyle={{ width: 200, height: 100 }}
+          title="-"
+          onPress={() => {
+            setResult(result - 1);
+          }}
+          buttonStyle={{ width: 100, height: 100 }}
           buttonType={ButtonTypes.OPERATOR}
         />
         {/* <Button
