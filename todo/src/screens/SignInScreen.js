005511a5ff7087d806d0137c1db6d17e8f8eb0ff
moonyeju <moonyeju8510@gmail.com>
2023-05-02 15:22:24 +0900

    4.4::이미지 크기 조절하기

---
+++ todo/src/screens/SignInScreen.js
@@ -0,0 +1,25 @@
+import { Image, Text, StyleSheet, View } from 'react-native';
+
+const SignInScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/main.png')}
+        style={styles.image}
+        resizeMode={'cover'}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
+export default SignInScreen;
